89587182d5356ce1dab0115655aaa2781f77c3e1
Logeshwari-0715 <86481524+Logeshwari-0715@users.noreply.github.com>
2021-06-26 19:42:35 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+  return (
+    <footer>
+    <p>Copyrights by shapeai @{new Date().getFullYear()}</p>
+    </footer>
+);
+}
+
+export default Footer;
